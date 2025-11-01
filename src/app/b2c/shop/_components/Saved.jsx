17e07a7815348ea9045ac8269c357c118206057f
towfiqislambd <towfiqislambd@gmail.com>
2025-11-01 12:33:47 +0600

    working in save page

--- src/app/b2c/shop/_components/Saved.jsx
+++ src/app/b2c/shop/_components/Saved.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React from "react";
+import doorImg from "../../../../Assets/d1.jpg";
+import Image from "next/image";
+const data = [
+  { id: 1, img: doorImg },
+  { id: 2, img: doorImg },
+  { id: 3, img: doorImg },
+  { id: 4, img: doorImg },
+  { id: 5, img: doorImg },
+  { id: 6, img: doorImg },
+];
 
 const Saved = () => {
-  return <div>Saved</div>;
+  return (
+    <section className="p-6 rounded-lg shadow-lg bg-white">
+      <h2 className="text-xl font-medium text-primary-text mb-4">Saved</h2>
+
+      <div className="grid grid-cols-3 gap-6">
+        {data?.map(item => (
+          <figure className="rounded-2xl relative h-[350px] shadow-lg  overflow-hidden group">
+            <Image
+              src={item?.img}
+              alt="door_img"
+              fill
+              unoptimized
+              className="rounded-2xl group-hover:scale-105 duration-700"
+            />
+          </figure>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Saved;
