75361cdeb90bfe830012ca00236543880c460c0d
towfiqislambd <towfiqislambd@gmail.com>
2025-11-04 12:42:37 +0600

    completed dashboard layout

---
+++ src/app/dashboard/b2b/guest-code/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <div>B2B guest-code</div>;
+};
+
+export default page;
---
+++ src/app/dashboard/b2b/user-management/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <div>B2B user-management</div>;
+};
+
+export default page;
