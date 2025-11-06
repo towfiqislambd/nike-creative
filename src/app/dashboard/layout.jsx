23c5b6f5c3dc4cac9c6d39f210b0df4fe67bc71e
towfiqislambd <towfiqislambd@gmail.com>
2025-11-06 16:55:52 +0600

    updated link

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -219,6 +219,7 @@ const adminSidebarLinks = [
 export default function DashboardLayout({ children }) {
   const user = { role: "b2b" };
 
+  
   return (
     <section className="flex min-h-screen max-h-screen">
       {/* Sidebar */}
